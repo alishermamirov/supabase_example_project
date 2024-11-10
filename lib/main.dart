55baab3e9cac7904af41261fd707d67a89fed248
Alisher Mamirov <alishermamirov452@gmail.com>
2024-11-10 18:51:41 +0500

    supabase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_example_project/screens/notes_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "https://vcgcfaezavetucbxfcwp.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZ2NmYWV6YXZldHVjYnhmY3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyNDY0MDAsImV4cCI6MjA0NjgyMjQwMH0.kNOPtv2XuvD-sX-ZDX2esX-Qd5FG0GiKAq3_2N2nzLw",
+  );
   runApp(MyApp());
 }
 
@@ -9,6 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NotesScreen(),
+    );
   }
 }
