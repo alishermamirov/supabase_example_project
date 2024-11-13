9073039df5156ce4710dae1fad0f7f07414ae186
Alisher Mamirov <alishermamirov452@gmail.com>
2024-11-13 20:15:39 +0500

    note save and get added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_example_project/screens/notes_screen.dart';
+import 'screens/notes_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: "https://vcgcfaezavetucbxfcwp.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZ2NmYWV6YXZldHVjYnhmY3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyNDY0MDAsImV4cCI6MjA0NjgyMjQwMH0.kNOPtv2XuvD-sX-ZDX2esX-Qd5FG0GiKAq3_2N2nzLw",
+    url: "https://rfquurjzjbhczxjdncdh.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcXV1cmp6amJoY3p4amRuY2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MDg4NzUsImV4cCI6MjA0NzA4NDg3NX0.1xcLw1S1SyLfIHdctyOuT7CedSZZbnrk1tTfV8h1jq4",
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: NotesScreen(),
     );
--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class NotesScreen extends StatelessWidget {
+class NotesScreen extends StatefulWidget {
   const NotesScreen({super.key});
 
   @override
+  State<NotesScreen> createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  final noteController = TextEditingController();
+
+  void showAddNote(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Add note"),
+          content: TextField(
+            controller: noteController,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                saveNote();
+                Navigator.pop(context);
+              },
+              child: Text("Add"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //save note to supabase
+  void saveNote() async {
+    await Supabase.instance.client
+        .from("notes")
+        .insert({"body": noteController.text});
+  }
+
+  // get notes from supabase
+
+  final noteStream =
+      Supabase.instance.client.from("notes").stream(primaryKey: ["id"]);
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Supabase example"),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+        stream: noteStream,
+        builder: (context, snapshot) {
+          print(snapshot.data);
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            final data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final note = data[index];
+
+                return ListTile(
+                  title: Text(note["body"]),
+                );
+              },
+            );
+          }
+          return Center(
+            child: Container(
+              child: Text("Malumot mavjud emas"),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showAddNote(context),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
